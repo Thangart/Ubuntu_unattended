f5aced2894f07c16a4b474dcbc0a0e903a8831e4
Thangart <Stefan3459@gmail.com>
2018-01-01 23:57:10 +0100

    Version 1.0 working script. Changed the folder structure so it can be copied over the extracted image

---
+++ preseed/firstrun.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+if [ ! -f /var/tmp/toggle ];
+    then
+        sleep 100
+        touch /var/tmp/toggle
+        wget --no-check-certificate -O /var/tmp/post-install.sh "https://raw.githubusercontent.com/Thangart/Ubuntu_unattended/master/post-install.sh"
+        chmod +x /var/tmp/post-install.sh
+        /bin/sh /var/tmp/post-install.sh
+fi
